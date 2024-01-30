bce6268e6bf31031b2e02a2de6aff95b36e300b0
evie-song <evie.shuangsong@gmail.com>
2024-01-30 15:55:43 -0600

    add basic layout

---
+++ components/header.js
@@ -0,0 +1,33 @@
+import styles from "../styles/header.module.css";
+
+const Header = () => {
+  return (
+    <div className={`${styles.container} justify-content-between`}>
+      <div className="text-primary">Logo</div>
+      <div className="d-flex flex-grow-1 justify-content-end align-items-center">
+        <a>
+          <span className={styles.counter}>01.</span>
+          <span className={styles.title}>About</span>
+        </a>
+        <a>
+          <span className={styles.counter}>02.</span>
+          <span className={styles.title}>Experience</span>
+        </a>
+        <a>
+          <span className={styles.counter}>03.</span>
+          <span className={styles.title}>Work</span>
+        </a>
+        <a>
+          <span className={styles.counter}>04.</span>
+          <span className={styles.title}>Contact</span>
+        </a>
+        <div className={styles.resumeWrapper}>
+          <span className={styles.resumeTitle}>Resume</span>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
