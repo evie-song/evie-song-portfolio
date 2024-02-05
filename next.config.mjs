a5a9a10b87015cec1e7760099270756f7860c563
evie-song <evie.shuangsong@gmail.com>
2024-02-05 17:14:24 -0600

    updated basepath for deploying on vercel2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/evie-song-portfolio.vercel.app",
+  basePath: "/evie-song-portfolio-ksgrxjzp1-evies-projects-799694ff.vercel.app",
   output: "export",
   reactStrictMode: true,
 };
