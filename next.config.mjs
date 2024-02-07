329c2e333f7f60ba836aae1d39663693708373d7
evie-song <evie.shuangsong@gmail.com>
2024-02-07 11:44:50 -0600

    add responsive layout for project component

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = process.env.NODE_ENV === 'production' ? {
   basePath: "/evie-song-portfolio",
   output: "export",
+  // reactStrictMode: true,
+  images: { unoptimized: true}
+} :
+{
+  basePath: "",
+  // output: "export",
   // reactStrictMode: true,
   images: { unoptimized: true}
 };
